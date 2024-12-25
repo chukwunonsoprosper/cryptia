c607c818709e8ddaa2761f9d82c54e7854817ce3
davistheweb <121519171+davistheweb@users.noreply.github.com>
2024-12-25 12:33:21 +0100

    added more alphabets for algorthim

--- core/engine.js
+++ core/engine.js
@@ -1,9 +1,9 @@
 const Algorithm = () => {
-    return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?`~!@#$%^&*()-_=+,\'.,/\"{[:;"|€¥•≠≈‰…¿¡‘₩₽¢}]1234567890 ';
+    return 'abcdefghijklmnopqrstuvwxyzŵèéêëēėęŷÿûüùúūîïíīįìôöòóœøōõłßśšàáâäæãåāßśškłžżźçćčñńABCDEFGHIJKLMNOPQRSTUVWXYZŴÈÉÊËĒĖĘŶŸÛÜÙÚŪÎÏÍĪĮÌÔÖÒÓŒØŌÕŁSSŚŠÀÁÂÄÆÃÅĀSSŚŠKŁŽŻŹÇĆČÑŃ?`~!@#$%^&*()-_=+,\'.,/\"{[:;"|€¥•≠≈‰…¿¡‘₩₽¢}]1234567890 ';
 };
 
 const decryptionAlgorithm = () => {
-    return 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM?`~!@#$%^&*()-_=+,\'.,/\"{[:;"|€¥•≠≈‰…¿¡‘₩₽¢}]0987654321 ';
+    return 'qwertyuiopasdfghjklzxcvbnmŵèéêëēėęŷÿûüùúūîïíīįìôöòóœøōõłßśšàáâäæãåāßśškłžżźçćčñńQWERTYUIOPASDFGHJKLZXCVBNMŴÈÉÊËĒĖĘŶŸÛÜÙÚŪÎÏÍĪĮÌÔÖÒÓŒØŌÕŁSSŚŠÀÁÂÄÆÃÅĀSSŚŠKŁŽŻŹÇĆČÑŃ?`~!@#$%^&*()-_=+,\'.,/\"{[:;"|€¥•≠≈‰…¿¡‘₩₽¢}]0987654321 ';
 }
 
 export { Algorithm, decryptionAlgorithm }
